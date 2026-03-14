b49aee4031e9501c9b38e673c82cb405da4ad9fe
omdevsoni-svg <omdevsoni@gofynd.com>
2026-03-14 16:59:42 +0530

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import BottomNav from "./BottomNav";
+import TopBar from "./TopBar";
 
 const AppLayout = () => {
   return (
     <div className="min-h-screen bg-background">
+      <TopBar />
       <main className="pb-20">
         <Outlet />
       </main>
